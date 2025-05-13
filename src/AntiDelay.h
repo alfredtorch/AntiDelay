6d5f2e63f7bd217580037627d3b30efb5c7021bf
Max <38537119+alfredtorch@users.noreply.github.com>
2025-05-13 08:27:59 +0200

    Update AntiDelay.h
    elapsed & remnaining

--- src/AntiDelay.h
+++ src/AntiDelay.h
@@ -3,12 +3,11 @@
  * @author Martin Vichnál
  * @page https://github.com/martinvichnal/AntiDelay
  * @brief AntiDelay is a library that aims to provide non-blocking delay functionality.
- * @version v1.1.3
+ * @version v1.1.2
  * @date 2023-12-31
  *
  * @copyright Copyright (c) 2024
  */
-
 
 #ifndef AntiDelay_h
 #define AntiDelay_h
@@ -17,20 +16,25 @@
 
 class AntiDelay
 {
-  public:
-    AntiDelay(unsigned long interval);
-    operator bool();
-    void reset();
-    void setInterval(unsigned long interval);
-    void pause();
-    void resume();
-    bool isRunning();
+public:
+  AntiDelay(unsigned long interval);
+  operator bool();
+  void reset();
+  void setInterval(unsigned long interval);
+  void pause();
+  void resume();
+  bool isRunning();
 
-  private:
-    unsigned long timeInterval;
-    unsigned long previousMillis;
-    bool isPaused;
-    unsigned long pauseOffset;
+  /** temps écoulé depuis le dernier reset (ms) */
+  uint32_t elapsed() const;
+
+  /** temps restant avant l’expiration du timer (ms) */
+  uint32_t remaining() const;
+
+private:
+  unsigned long timeInterval;   // durée programmée
+  unsigned long previousMillis; // instant du dernier reset
+  bool isPaused;
+  unsigned long pauseOffset;
 };
-
 #endif
